37dbbf3f3c2b6cdf06218a938e42db47c2bf8eaf
akisute <akisutesama@gmail.com>
2013-11-30 15:40:47 +0900

    Updated web cell documentation. Reverted previous commit, was wrong because cells are reused.

--- TwitterClient/TwitterPicturesViewWebCell.m
+++ TwitterClient/TwitterPicturesViewWebCell.m
@@ -19,30 +19,42 @@
 - (void)onTweetUpdated
 {
     [super onTweetUpdated];
+    
+    /*
+     Strategy:
+     onTweetUpdated is only called when self.tweet is changed. This means the implementation here will never be called on the same tweet twice, but
+     the cell itself will be reused on other tweets.
+     
+     When scrolling fast enough, this method can be called while TwitterMediaCache is stil rendering a snapshot for old self.tweet.
+     In this case, we need to cancel previous request then ask for new snapshot.
+     
+     Known Problem:
+     Cancel is not available right now.
+     To solve this, snapshotViewWithRenderRequest:completionHandler: should return request ID for later use. Add a cancel request method
+     to media cache. ImageCell also require this.
+     */
     MTweetURLObject *urlObject = self.tweet.urls.firstObject;
     if (urlObject) {
-        if (self.snapshotView) {
-            // Do nothing. snapshot is already taken and attached.
-        } else {
-            self.URLLabel.hidden = NO;
-            self.URLLabel.text = urlObject.displayURLString;
-            [[TwitterMediaCache sharedCache] snapshotViewWithRenderRequest:[TwitterWebRenderRequest renderRequestWithURL:urlObject.url mode:TwitterWebRenderRequestModeSquareTopLeft]
-                                                         completionHandler:^(UIView *view) {
-                                                             dispatch_async(dispatch_get_main_queue(), ^{
-                                                                 if (view) {
-                                                                     self.URLLabel.hidden = YES;
-                                                                     [self.snapshotView removeFromSuperview];
-                                                                     self.snapshotView = view;
-                                                                     self.snapshotView.frame = self.contentView.bounds;
-                                                                     self.snapshotView.contentMode = UIViewContentModeCenter;
-                                                                     [self.contentView addSubview:self.snapshotView];
-                                                                 } else {
-                                                                     [self.snapshotView removeFromSuperview];
-                                                                     self.snapshotView = nil;
-                                                                 }
-                                                             });
-                                                         }];
-        }
+        self.URLLabel.hidden = NO;
+        self.URLLabel.text = urlObject.displayURLString;
+        [self.snapshotView removeFromSuperview];
+        self.snapshotView = nil;
+        [[TwitterMediaCache sharedCache] snapshotViewWithRenderRequest:[TwitterWebRenderRequest renderRequestWithURL:urlObject.url mode:TwitterWebRenderRequestModeSquareTopLeft]
+                                                     completionHandler:^(UIView *view) {
+                                                         dispatch_async(dispatch_get_main_queue(), ^{
+                                                             if (view) {
+                                                                 self.URLLabel.hidden = YES;
+                                                                 self.URLLabel.text = nil;
+                                                                 self.snapshotView = view;
+                                                                 self.snapshotView.frame = self.contentView.bounds;
+                                                                 self.snapshotView.contentMode = UIViewContentModeCenter;
+                                                                 [self.contentView addSubview:self.snapshotView];
+                                                             } else {
+                                                                 [self.snapshotView removeFromSuperview];
+                                                                 self.snapshotView = nil;
+                                                             }
+                                                         });
+                                                     }];
     } else {
         self.URLLabel.hidden = YES;
         self.URLLabel.text = nil;
